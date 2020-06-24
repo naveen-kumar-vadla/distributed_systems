c0028b6b846718d1db790788c6deb696dcdb40fb
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-06-24 14:40:01 +0530

    added worker to it who takes the request from server and serves it and responds with iam done and server issues new job to the worker if there is any job

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
+const http = require('http');
 const express = require('express');
 const app = express();
 const processImages = require('./processImages').processImages;
+
+let id = 0;
+let isWorkerFree = true;
+const jobs = []; // { id: id, params: params }
+setInterval(() => {
+  if (isWorkerFree && jobs.length) {
+    const job = jobs.shift();
+    console.log('Scheduling jon on worker : ', job.id);
+    delegateToWorker(job.id, job.params);
+  }
+}, 1000);
+
+const getWorkerOptions = () => {
+  return {
+    host: 'localhost',
+    port: '5000',
+    method: 'post',
+  };
+};
+
+const delegateToWorker = (id, { width, height, tags, count }) => {
+  const options = getWorkerOptions();
+  options.path = `/process/${id}/${count}/${width}/${height}/${tags}`;
+  const req = http.request(options, res => {
+    console.log('Got from worker', res.statusCode);
+  });
+  req.end();
+  isWorkerFree = false;
+};
 
 //log request url and method
 app.use((req, res, next) => {
@@ -8,16 +38,18 @@ app.use((req, res, next) => {
   next();
 });
 
+// app.get('/status/:id', (req, res) => {});
+
+app.post('/completed-job/:id', (req, res) => {
+  isWorkerFree = true;
+  res.end();
+});
+
 app.post('/process/:name/:count/:width/:height/:tags', (req, res) => {
-  processImages(req.params)
-    .then(tags => {
-      console.log(tags);
-      return tags;
-    })
-    .then(tags => {
-      res.write(JSON.stringify(tags));
-      res.end();
-    });
+  res.send(`id:${id}`);
+  res.end();
+  jobs.push({ id, params: req.params });
+  id++;
 });
 
 app.listen(8000, () => console.log('listening on 8000...'));
---
+++ worker.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const processImages = require('./processImages').processImages;
+
+const PORT = 5000;
+
+const getServerOptions = () => {
+  return {
+    host: 'localhost',
+    port: '8000',
+    method: 'post',
+  };
+};
+
+const informWorkerFree = ({ id, tags }) => {
+  const options = getServerOptions();
+  options.path = `/completed-job/${id}`;
+  const req = http.request(options, res => {});
+  req.end();
+};
+
+//log request url and method
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.post('/process/:id/:count/:width/:height/:tags', (req, res) => {
+  res.end();
+  processImages(req.params)
+    .then(tags => {
+      console.log(tags);
+      return { id: req.params.id, tags };
+    })
+    .then(informWorkerFree);
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}...`));
