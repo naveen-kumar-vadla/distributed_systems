9f9b4d15d36f9b64a347c7fc04627b1462ec70c9
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-07-01 11:30:25 +0530

    added script to load multiple jobs at once

---
+++ runJobs.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_00
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_01
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_02
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_03
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_04
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_05
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_06
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_07
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_08
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_09
+
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_10
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_11
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_12
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_13
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_14
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_15
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_16
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_17
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_18
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_19
+
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_20
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_21
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_22
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_23
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_24
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_25
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_26
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_27
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_28
+curl -X POST http://localhost:8000/process/amar/1000/1000/1000/hello_everyone_how_are_you_29
